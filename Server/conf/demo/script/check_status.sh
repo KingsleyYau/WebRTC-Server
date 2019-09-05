f21097417761ec1a631588e3aa02f7bc79647a4a
Kingsley Yau <Kingsleyyau@gmail.com>
2019-09-05 14:08:12 +0800

    修改检测脚本

--- Server/conf/demo/script/check_status.sh
+++ Server/conf/demo/script/check_status.sh
@@ -2,10 +2,12 @@
 # Watch mediaserver script
 # Author:	Max.Chiu
 
-watch -n 1 -c "ps -ef | grep -v tail | grep -v grep | grep -v js | grep 'mediaserver' && \
+watch -n 1 -c "ps -ef | grep -v tail | grep -v grep | grep -v js | grep -v ffmpeg | grep 'mediaserver' && \
 echo "-------------------- 		top		 --------------------" && \
 top -b -n 1 | head -n 6 && \
-top -b -n 1 | grep 'mediaserver\|turnserver\|ffmpeg' && \
+top -b -n 1 | grep 'mediaserver\|turnserver\|ffmpeg' | grep -v 0.0 && \
 echo "-------------------- 	netstat	 --------------------" && \
-netstat -anpult | grep 'mediaserver\|turnserver\|ffmpeg' \
+echo "TCP Socket:" && netstat -anplt | grep 'mediaserver\|turnserver\|ffmpeg' | wc -l && \
+echo "UDP Socket:" && netstat -anpu | grep 'mediaserver\|turnserver\|ffmpeg' | wc -l && \
+echo "Network:" && sar -n DEV 1 1 | grep Average \
 "
