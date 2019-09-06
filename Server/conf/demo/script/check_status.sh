50ab786307df023bfade30e1ad38aaf3b569c77a
Kingsley Yau <Kingsleyyau@gmail.com>
2019-09-06 11:56:06 +0800

    修改检测脚本

--- Server/conf/demo/script/check_status.sh
+++ Server/conf/demo/script/check_status.sh
@@ -2,12 +2,11 @@
 # Watch mediaserver script
 # Author:	Max.Chiu
 
-watch -n 1 -c "ps -ef | grep -v tail | grep -v grep | grep -v js | grep -v ffmpeg | grep 'mediaserver' && \
-echo "-------------------- 		top		 --------------------" && \
-top -b -n 1 | head -n 6 && \
-top -b -n 1 | grep 'mediaserver\|turnserver\|ffmpeg' | grep -v 0.0 && \
-echo "-------------------- 	netstat	 --------------------" && \
-echo "TCP Socket:" && netstat -anplt | grep 'mediaserver\|turnserver\|ffmpeg' | wc -l && \
-echo "UDP Socket:" && netstat -anpu | grep 'mediaserver\|turnserver\|ffmpeg' | wc -l && \
-echo "Network:" && sar -n DEV 1 1 | grep Average \
+watch -n 1 -c "ps -ef | grep -v tail | grep -v grep | grep -v js | grep -v ffmpeg | grep 'mediaserver';\
+echo "-------------------- 		top		 --------------------";\
+top -b -n 1 | head -n 6;\
+top -b -n 1 | grep 'mediaserver\|turnserver\|ffmpeg' | grep -v 0.0;\
+echo "-------------------- 	netstat	 --------------------";\
+echo "TCP Socket:" && netstat -anplt | grep 'mediaserver\|turnserver\|ffmpeg' | wc -l;\
+echo "UDP Socket:" && netstat -anpu | grep 'mediaserver\|turnserver\|ffmpeg' | wc -l;\
 "
--- Server/conf/local/script/check_status.sh
+++ Server/conf/local/script/check_status.sh
@@ -2,11 +2,13 @@
 # Watch mediaserver script
 # Author:	Max.Chiu
 
-watch -n 1 -c "ps -ef | grep -v tail | grep -v grep | grep -v js | grep -v ffmpeg | grep 'mediaserver' && \
+watch -n 1 -c "ps -ef | grep -v tail | grep -v grep | grep -v js | grep -v ffmpeg | grep 'mediaserver';\
 echo "-------------------- 		top		 --------------------" && \
-top -b -n 1 | head -n 6 && \
-top -b -n 1 | grep 'mediaserver\|turnserver\|ffmpeg' && \
-echo "-------------------- 	netstat	 --------------------" && \
-echo "TCP Open Files:" && netstat -anplt | grep 'mediaserver\|turnserver\|ffmpeg' | wc -l && \
-echo "UDP Open Files:" && netstat -anpu | grep 'mediaserver\|turnserver\|ffmpeg' | wc -l \
+top -b -n 1 | head -n 6;\
+top -b -n 1 | grep 'mediaserver\|turnserver\|ffmpeg' | grep -v 0.0;\
+echo "-------------------- 	netstat	 --------------------";\
+echo "TCP Socket:" && netstat -anplt | grep 'mediaserver\|turnserver\|ffmpeg' | wc -l;\
+echo "UDP Socket:" && netstat -anpu | grep 'mediaserver\|turnserver\|ffmpeg' | wc -l;\
+echo "-------------------- 	sar	 --------------------";\
+echo "Network:" && sar -n DEV 1 1 | grep Average:\
 "
