a6ef7f661b65a7271bd8953d5af23a3ba8fdb042
Kingsley Yau <Kingsleyyau@gmail.com>
2019-09-18 09:16:55 +0800

    删除多余抓包

--- Server/MediaServer/pcap/play_rtmp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Play RTMP Stream script
-# Author:	Max.Chiu
-
-FFMPEG=/Users/max/Documents/tools/build/bin/ffmpeg
-FFPLAY=/Users/max/Documents/tools/build/bin/ffplay
-
-$FFPLAY -i rtmp://172.25.32.17:19351/live/max0
--- Server/MediaServer/pcap/play_rtp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Play RTP Stream script
-# Author:	Max.Chiu
-
-FFMPEG=/Users/max/Documents/tools/build/bin/ffmpeg
-FFPLAY=/Users/max/Documents/tools/build/bin/ffplay
-
-$FFPLAY -i rtp.sdp
