4a7a98e2b0bcea88b7949752b33eca90d9bff684
Kingsley Yau <Kingsleyyau@gmail.com>
2019-10-22 16:06:58 +0800

    增加打包脚本，增加针对camshare的上传和播放

---
+++ Server/package.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# Pakcage mediasever script
+# Author:	Max.Chiu
+
+ENVS=(local demo product)
+for ENV in ${ENVS[@]};do
+	echo "############## Start packaging [$ENV] ##############"
+	
+	# Copy Executable Files
+	mkdir -p tmp/$ENV/bin/
+	cp -rf build/bin/ffmpeg tmp/$ENV/bin/
+	cp -rf build/bin/mediaserver tmp/$ENV/bin/
+	cp -rf build/bin/turnadmin tmp/$ENV/bin/
+	cp -rf build/bin/turnserver tmp/$ENV/bin/
+	
+	# Copy Config Files
+	mkdir -p tmp/$ENV/etc/
+	cp -rf conf/$ENV/etc tmp/$ENV/etc/
+	mkdir -p tmp/$ENV/script/
+	cp -rf conf/$ENV/script tmp/$ENV/script/
+	mkdir -p tmp/$ENV/var/
+	cp -rf conf/$ENV/var tmp/$ENV/var/
+	
+	mkdir -p package
+	tar zcvf package/$ENV.tar.gz tmp/$ENV/
+	
+	echo "############## Finish packaging [$ENV] ##############"
+	echo "# Package file: package/$ENV.tar.gz"
+	echo ""
+done
