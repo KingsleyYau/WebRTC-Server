9d9bc8a42474c7b50f463bd74212772efce0a945
Kingsley Yau <Kingsleyyau@gmail.com>
2019-11-12 16:38:41 +0800

    Update restart_all_service.sh

--- Server/conf/local/script/restart_all_service.sh
+++ Server/conf/local/script/restart_all_service.sh
@@ -4,19 +4,23 @@
 # Date: 2019/12/11
 
 APP_DIR=/root/Max/mediaserver
-SCRIPT_DIR=$APP_DIR/script
-cd $SCRIPT_DIR
+cd $APP_DIR
 
 # Start turnserver
 echo "# Turnserver starting......"
-#nohup ./start_turnserver.sh >/dev/null 2>&1 &
+#nohup ./script/start_turnserver.sh >/dev/null 2>&1 &
 echo "# Turnserver start OK"
 
 sleep 3
 
+# Stop mediaserver
+echo "# Mediaserver stoping......"
+./script/stop_mediaserver.sh
+echo "# Mediaserver stop OK"
+
 # Start mediaserver
 echo "# Mediaserver starting......"
-nohup ./start_mediaserver.sh >/dev/null 2>&1 &
+nohup ./script/start_mediaserver.sh >/dev/null 2>&1 &
 PID=$!
 echo "# Mediaserver start OK, pid: $PID"
 
