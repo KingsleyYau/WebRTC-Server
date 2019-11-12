a9968be656db6ec1a52b55078895310d0696adbf
Kingsley Yau <Kingsleyyau@gmail.com>
2019-11-12 15:20:12 +0800

    Update install.sh

--- Server/bin/install.sh
+++ Server/bin/install.sh
@@ -8,18 +8,18 @@ mkdir -p $DEST_PATH
 
 # Copy Executable Files
 cp -rf bin $DEST_PATH
-chmod -R 755 $DEST_PATH/bin/
+chmod -R 755 $DEST_PATH/bin/*
 
 # Copy Config Files
 cp -rf etc $DEST_PATH
-chmod -R 744 $DEST_PATH/etc/
+chmod -R 744 $DEST_PATH/etc/*
 
 # Copy Script Files
 cp -rf script $DEST_PATH
-chmod -R 755 $DEST_PATH/script/
+chmod -R 755 $DEST_PATH/script/*
 
 # Copy Var Files
 cp -rf var $DEST_PATH
-chmod -R 744 $DEST_PATH/var/
+chmod -R 744 $DEST_PATH/var/*
 
 echo "############## Finish installing mediaserver ##############"
