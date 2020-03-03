20f3f9342d88486bc53f04bfda424cf852268e35
Kingsley Yau <Kingsleyyau@gmail.com>
2020-03-03 14:30:57 +0800

    Update imagefilter.js

--- Server/sig/src/static/imagefilter.js
+++ Server/sig/src/static/imagefilter.js
@@ -63,6 +63,12 @@ class ImageFilter {
         this.gl = canvas.getContext('webgl');
         this.initFilterVertexBuffer(this.filterVertex_180);
         this.initShaders();
+
+        this.imgTexture = this.gl.createTexture();
+        this.gl.bindTexture(this.gl.TEXTURE_2D, this.imgTexture);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR);
     }
 
     initShaders() {
@@ -99,36 +105,38 @@ class ImageFilter {
     }
 
     draw() {
-        this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
 
         this.gl.useProgram(this.programObject);
 
-        this.gl.activeTexture(this.gl.TEXTURE0);
-        this.gl.bindTexture(this.gl.TEXTURE_2D, this.imgTexture);
+        if ( this.canDraw ) {
+            this.gl.activeTexture(this.gl.TEXTURE0);
+            this.gl.bindTexture(this.gl.TEXTURE_2D, this.imgTexture);
 
-        var aPosition = this.gl.getAttribLocation(this.programObject, "aPosition");
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.glVertexBuffer);
-        this.gl.vertexAttribPointer(aPosition, 2, this.gl.FLOAT, false, 16, 0);
-        this.gl.enableVertexAttribArray(aPosition);
+            var aPosition = this.gl.getAttribLocation(this.programObject, "aPosition");
+            this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.glVertexBuffer);
+            this.gl.vertexAttribPointer(aPosition, 2, this.gl.FLOAT, false, 16, 0);
+            this.gl.enableVertexAttribArray(aPosition);
 
-        var aTextureCoordinate = this.gl.getAttribLocation(this.programObject, "aTextureCoordinate");
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.glVertexBuffer);
-        this.gl.vertexAttribPointer(aTextureCoordinate, 2, this.gl.FLOAT, false, 16, 8);
-        this.gl.enableVertexAttribArray(aTextureCoordinate);
+            var aTextureCoordinate = this.gl.getAttribLocation(this.programObject, "aTextureCoordinate");
+            this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.glVertexBuffer);
+            this.gl.vertexAttribPointer(aTextureCoordinate, 2, this.gl.FLOAT, false, 16, 8);
+            this.gl.enableVertexAttribArray(aTextureCoordinate);
 
-        this.uInputTexture = this.gl.getUniformLocation(this.programObject, "uInputTexture");
-        this.uImagePixel = this.gl.getUniformLocation(this.programObject, "uImagePixel");
-        this.uOffet = this.gl.getUniformLocation(this.programObject, "uOffet");
+            this.uInputTexture = this.gl.getUniformLocation(this.programObject, "uInputTexture");
+            this.uImagePixel = this.gl.getUniformLocation(this.programObject, "uImagePixel");
+            this.uOffet = this.gl.getUniformLocation(this.programObject, "uOffet");
 
-        // 填充着色器-颜色采样器, 将纹理单元GL_TEXTURE0绑元定到采样器
-        this.gl.uniform1i(this.uInputTexture, 0);
+            // 填充着色器-颜色采样器, 将纹理单元GL_TEXTURE0绑元定到采样器
+            this.gl.uniform1i(this.uInputTexture, 0);
+            this.gl.uniform2f(this.uImagePixel, 1 / 720.0, 1 / 1080.0);
 
-        this.gl.uniform2f(this.uImagePixel, 1 / 720.0, 1 / 1080.0);
+            var num = Math.random() * 10 % 5;
+            this.gl.uniform2f(this.uOffet, num, num);
 
-        var num = Math.random() * 10 % 5;
-        this.gl.uniform2f(this.uOffet, num, num);
+            this.gl.drawArrays(this.gl.TRIANGLES, 0, 6);
+        }
 
-        this.gl.drawArrays(this.gl.TRIANGLES, 0, 6);
     }
 
     initFilterVertexBuffer(filterVertex) {
@@ -139,7 +147,6 @@ class ImageFilter {
     }
 
     loadImage(url) {
-        this.imgTexture = this.gl.createTexture();
         this.gl.bindTexture(this.gl.TEXTURE_2D, this.imgTexture);
         this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
         this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
@@ -149,44 +156,14 @@ class ImageFilter {
         img.onload = function() {
             this.gl.bindTexture(this.gl.TEXTURE_2D, this.imgTexture);
             this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.gl.RGBA, this.gl.UNSIGNED_BYTE, img);
+            this.canDraw = true;
         }.bind(this);
         img.src = url;
     }
-    genPixelTexture() {
-//         // 创建新纹理
-//         int textureId[] = new int[1];
-//         GLES20.glGenTextures(1, textureId, 0);
-//
-//         // 绑定纹理
-//         this.gl.glBindTexture(GLES20.GL_TEXTURE_2D, textureId[0]);
-//
-//         /**
-//          * GL_LINEAR 不失真, 但会模糊
-//          * GL_NEAREST 失真, 但不会模糊
-//          *
-//          * GL_REPEAT 平铺
-//          * GL_MIRRORED_REPEAT 镜像平铺
-//          * GL_CLAMP_TO_EDGE 超出部分变为条纹
-//          * GL_CLAMP_TO_BORDER 居中
-//          */
-//         // 设置纹理参数
-//         GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
-//             GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST);
-//         GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
-//             GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
-//         GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
-//             GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
-//         GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
-//             GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
-//
-//         // 解除纹理绑定
-//         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
-//
-// //        Log.d(LSConfig.TAG, String.format("LSImageFilter::genPixelTexture( textureId : %d )", textureId[0]));
-//
-//         String method = String.format("genPixelTexture( textureId : %d )", textureId[0]);
-//         checkGLError(method, null);
-//
-//         return textureId;
+
+    drawImage(img) {
+        this.gl.bindTexture(this.gl.TEXTURE_2D, this.imgTexture);
+        this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.gl.RGBA, this.gl.UNSIGNED_BYTE, img);
+        this.canDraw = true;
     }
 }
