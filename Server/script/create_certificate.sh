96d6ab0d52d658356bac190d61f421bc075852bf
Kingsley Yau <Kingsleyyau@gmail.com>
2020-09-18 09:50:18 +0800

    修改证书脚本

--- Server/script/create_certificate.sh
+++ Server/script/create_certificate.sh
@@ -6,27 +6,22 @@
 # Create output directory
 mkdir output
 
-rm output/index.txt -rf
-touch output/index.txt
-rm output/index -rf
-echo 01 > output/serial
-
 # Create CA key
 openssl genrsa -out output/private/cakey.pem 2048
 # Create CA certificate
-openssl req -new -x509 -days 3650 -key output/private/cakey.pem -out output/private/cacert.pem -subj /C=CN/ST=Hongkong/L=Hongkong/O=EST/OU=EST/CN=EST/emailAddress=max.chiu@qpidnetwork.com -extensions v3_ca
+openssl req -new -x509 -nodes -key output/private/cakey.pem -days 824 -sha256 -out output/private/cacert.crt -subj /C=CN/ST=Hongkong/L=Hongkong/O=EST/OU=EST/CN=EST/emailAddress=max.chiu@qpidnetwork.com -extensions v3_ca
 # Create User key
 openssl genrsa -out output/server.key 2048
 # Create User request
-openssl req -new -key output/server.key -out output/server.req -subj /C=CN/ST=Hongkong/L=Hongkong/O=mediaserver/OU=mediaserver/CN=mediaserver/emailAddress=kingsleyyau@gmail.com
+openssl req -new -key output/server.key -config cert.cnf -out output/server.csr
 # Signed User certificate
-openssl ca -in output/server.req -days 3650 -out output/server.crt -config openssl.cnf -extensions v3_req
+openssl x509 -req -in output/server.csr -CA output/private/cacert.crt -CAkey output/private/cakey.pem -CAcreateserial -out output/server.crt -days 824 -sha256 -extfile cert.cnf -extensions req_ext
 
 # Create certificate with interactive model
 #openssl req -x509 -nodes -newkey rsa:1024 -keyout output/webrtc_dtls.key -out output/webrtc_dtls.crt
 # Create certificate with non-interactive model
-#openssl req -x509 -days 3650 -nodes -newkey rsa:1024 -keyout output/webrtc_dtls.key -out output/webrtc_dtls.crt -subj /C=CN/ST=Hongkong/L=Hongkong/O=mediaserver/OU=mediaserver/CN=mediaserver/emailAddress=max.chiu@qpidnetwork.com
-#openssl req -x509 -days 3650 -nodes -newkey rsa:1024 -keyout output/server.key -out output/server.crt -subj /C=CN/ST=Hongkong/L=Hongkong/O=mediaserver/OU=mediaserver/CN=mediaserver/emailAddress=max.chiu@qpidnetwork.com
+#openssl req -x509 -days 824 -nodes -newkey rsa:1024 -keyout output/webrtc_dtls.key -out output/webrtc_dtls.crt -subj /C=CN/ST=Hongkong/L=Hongkong/O=mediaserver/OU=mediaserver/CN=mediaserver/emailAddress=max.chiu@qpidnetwork.com
+#openssl req -x509 -days 824 -nodes -newkey rsa:1024 -keyout output/server.key -out output/server.crt -subj /C=CN/ST=Hongkong/L=Hongkong/O=mediaserver/OU=mediaserver/CN=mediaserver/emailAddress=max.chiu@qpidnetwork.com
 
 # Create private key with password
 #openssl genrsa -passout pass:123456 -des3 -out output/webrtc_dtls.key 1024
