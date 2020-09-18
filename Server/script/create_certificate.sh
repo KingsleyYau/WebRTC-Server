0d4a6b8b79e4271804429982bde665dcd0159e30
Kingsley Yau <Kingsleyyau@gmail.com>
2020-09-18 10:40:26 +0800

    Update create_certificate.sh

--- Server/script/create_certificate.sh
+++ Server/script/create_certificate.sh
@@ -7,15 +7,26 @@
 mkdir output
 
 # Create CA key
-openssl genrsa -out output/private/cakey.pem 2048
+#openssl genrsa -out output/private/cakey.pem 2048
 # Create CA certificate
-openssl req -new -x509 -nodes -key output/private/cakey.pem -days 824 -sha256 -out output/private/cacert.crt -subj /C=CN/ST=Hongkong/L=Hongkong/O=EST/OU=EST/CN=EST/emailAddress=max.chiu@qpidnetwork.com -extensions v3_ca
+#openssl req -new -x509 -nodes -key output/private/cakey.pem -days 824 -sha256 -out output/private/cacert.crt -subj /C=CN/ST=Hongkong/L=Hongkong/O=EST/OU=EST/CN=EST/emailAddress=max.chiu@qpidnetwork.com -extensions v3_ca
+
+
 # Create User key
 openssl genrsa -out output/server.key 2048
 # Create User request
 openssl req -new -key output/server.key -config cert.cnf -out output/server.csr
 # Signed User certificate
 openssl x509 -req -in output/server.csr -CA output/private/cacert.crt -CAkey output/private/cakey.pem -CAcreateserial -out output/server.crt -days 824 -sha256 -extfile cert.cnf -extensions req_ext
+
+
+# Verify User certificate
+openssl verify -CAfile output/private/cacert.crt output/server.crt 
+# Show User certificate info
+openssl x509 -in output/server.crt -text -noout
+# Check remote domain certificate
+openssl s_client -connect demo-stream2.charmlive.com:9082 -CAfile private/cacert.crt
+
 
 # Create certificate with interactive model
 #openssl req -x509 -nodes -newkey rsa:1024 -keyout output/webrtc_dtls.key -out output/webrtc_dtls.crt
