40dd1184541e82d88f1f9e6dc0d593ed4bac8486
Kingsley Yau <Kingsleyyau@gmail.com>
2020-10-19 11:31:40 +0800

    Update heartbeat-handler.js

--- Server/sig/src/router/im/client/handler/heartbeat-handler.js
+++ Server/sig/src/router/im/client/handler/heartbeat-handler.js
@@ -16,7 +16,7 @@ module.exports = class HeartBeatHandler extends BaseHandler {
     }
 
     static getRoute() {
-        return 'imRTC/heartbeat';
+        return 'imRTC/sendPing';
     }
 
     async handle(ctx, reqData) {
